428be5324c42b54f0fe864bd8370e1972921a60f
Hauke Rehfeld <hrehfeld@uni-koblenz.de>
2014-08-06 20:32:55 +0200

    cln: much cleaner kdtree
    --HG--
    extra : rebase_source : 40da65582442321f77cb40b1d4892478d4c4872a

--- kdtree.hpp
+++ kdtree.hpp
@@ -1,4 +1,5 @@
 
+#include <numeric>
 namespace spatial {
 
 	namespace helper {
@@ -6,7 +7,7 @@ namespace spatial {
 		int MinIndex(T const* const seq, int const length, int N, int* minIndex, T* min) {
 			for (int i = 0; i < N; ++i)
 			{
-				min[i] = FLT_MAX;
+				min[i] = ::std::numeric_limits<T>::max();
 				minIndex[i] = -1;
 			}
 
@@ -52,66 +53,60 @@ namespace spatial {
 	}
 
 	template<int D, typename T, typename T3D>
-	class KdTreeSearch;
-
-
-	template<int D = 3, typename T = float, typename T3D = ::glm::vec3>
 	class KdTree {
 		typedef KdTree<D, T, T3D> Tree;
 		typedef Tree Self;
 
-		template <int D, typename T, typename T3D>
-		friend class KdTreeSearch;
+		struct Index
+		{
+		private:
+			int i;
+			bool leaf;
 
-		class Node {
-			friend class Tree;
+		public:
+			Index()
+			{
+			}
+		private:
+			Index(int i, bool leaf)
+				: i(i)
+				, leaf(leaf)
+			{
+			}
+		public:
 
-			virtual bool IsLeaf() const = 0;
-			virtual void Add(T3D const& p, Tree & parent) = 0;
+			static inline Index make_leaf(int i)
+			{
+				return Index(-(i + 1), true);
+			}
+			static inline Index make_stem(int i)
+			{
+				return Index(i, false);
+			}
+			inline int from_leaf() const
+			{
+				assert(i < 0);
+				assert(leaf == i < 0);
+				return -i - 1;
+			}
+
+			inline bool is_leaf() const
+			{
+				assert(leaf == i < 0);
+				return i < 0;
+			}
+
+			inline int from_stem() const
+			{
+				assert(i >= 0);
+				assert(!leaf);
+				return i;
+			}
+
 		};
 
-		class Stem : public Node {
-			friend class Tree;
-			friend class KdTreeSearch<D, T, T3D>;
-
-			T splitValue;
-			int splitAxis;
-
-			Tree* left;
-			Tree* right;
-
-
-			Stem(Tree* left, Tree* right, T splitValue, int splitAxis)
-				: left(left)
-				, right(right)
-				, splitValue(splitValue)
-				, splitAxis(splitAxis)
-			{}
-
-			~Stem() {
-				delete left;
-				delete right;
-			}
-
-			inline bool IsLeft(T3D const& p) const { return p[splitAxis] < splitValue; }
-			bool IsLeaf() const { return false; }
-
-			void Add(T3D const& p, Tree & tree)
-			{
-				Add(p);
-			}
-
-			inline void Add(T3D const& p)
-			{
-				Tree* child = IsLeft(p) ? left : right;
-				child->Add(p);
-			}
-		};
-
-		class Leaf : public Node {
-			friend class Tree;
-			friend class KdTreeSearch<D, T, T3D>;
-
+		struct Leaf
+		{
 			// Bounds are only ever used when splitting
 			T3D bounds[2];
 			std::vector<T3D> bucket;
@@ -140,304 +135,374 @@ namespace spatial {
 
 
 
-			inline int GetSplitAxis()
+			inline int GetSplitAxis(T& range)
 			{
 				T3D extend = bounds[1] - bounds[0];
 				int axis = 0;
-				T range = extend[0];
+				range = extend[0];
 				for (int i = 1; i < D; ++i) {
 					if (extend[i] > range) {
 						axis = i;
 						range = extend[i];
 					}
 				}
-				if (range < 0.001f) //TODO: make this epsilon
-				{
-					return -1;
-				}
 				return axis;
 			}
 
-			virtual void Add(T3D const& p, Tree & tree)
+			bool Add(T3D const& p, int bucketsize /*Tree & tree*/)
 			{
 				bucket.empty() ?
 					InitBounds(p) : UpdateBounds(p);
+
+				//we already increased bucketsize, so account for that
+				while (bucket.size() >= bucketsize)
+				{
+					bucketsize *= 2;
+				}
+
 				bucket.push_back(p);
 
 				//dont need to split
-				if (bucket.size() < tree.bucketsize)
+				if (bucket.size() < bucketsize)
 				{
-					return;
+					return true;
 				}
-
-				//we need to split
-				assert(bucket.size() >= tree.bucketsize);
-
-				//double bucket size if no extend in any direction
-				//split axis returns -1 if we have no extend in any direction
-				int splitAxis = GetSplitAxis();
-				T const EPSILON = 0.0000001;
-				if (splitAxis < 0
-					//relies on bounds[1] being >= bounds[0] to save the abs
-					|| bounds[1][splitAxis] - bounds[0][splitAxis] <= EPSILON)
-				{
-					tree.bucketsize *= 2;
-					return;
-				}
-
-				T splitValue = (bounds[0][splitAxis] + bounds[1][splitAxis]) / 2;
-				assert(bucket.size() == tree.bucketsize);
-
-				// Don't let the split value be the same as the upper value as
-				// can happen due to rounding errors!
-				//if (splitValue == bounds[1][splitAxis])
-				//{
-				//	splitValue = bounds[0][splitAxis];
-				//}
-				assert(splitValue > bounds[0][splitAxis]);
-				assert(splitValue < bounds[1][splitAxis]);
+				return false;
+			}
 
 
-				//@todo use this node as new leafnode for left child
-				tree.stem = new Stem(new Tree(tree.bucketsize), new Tree(tree.bucketsize), splitValue, splitAxis);
 
+		};
+
+		struct Stem
+		{
+			T splitValue;
+			int splitAxis;
+
+			Index children[2];
+
+
+		private:
+			Stem()
+				: children({ 0, 0 })
+				, splitValue(0)
+				, splitAxis(0)
+			{
+			}
+		public:
+
+			/** split a leaf node */
+			Stem(int const splitAxis
+				, T const range
+				, Leaf& leaf
+				, Index const ileaf
+				, int const bucketsize
+				, ::std::vector<Stem>& stems
+				, ::std::vector<Leaf>& leafs
+				)
+				: splitAxis(splitAxis)
+			{
+				assert(ileaf.is_leaf());
+				assert(leaf.bucket.size() % bucketsize == 0);
+
+				splitValue = leaf.bounds[0][splitAxis] + range / T(2);
+				assert(splitValue > leaf.bounds[0][splitAxis]);
+				assert(splitValue < leaf.bounds[1][splitAxis]);
+
+				
+				//use old leaf as left child
+				children[0] = ileaf;
+				children[1] = Index::make_leaf(leafs.size());
+				leafs.emplace_back(bucketsize);
+
+				auto const old = leaf.bucket;
+
+				leaf.bucket.clear();
 				//move bucket contents into children
-				for (int i = 0; i < bucket.size(); ++i)
+				for (auto const& p: old)
 				{
-					tree.stem->Add(bucket[i], tree);
+					auto const c = int(is_upper(p));
+					auto& child = children[c];
+					leafs[child.from_leaf()].bucket.emplace_back(::std::move(p));
 				}
+			}
 
-				//replace ourselves with new stem
-				delete tree.leaf;
-				tree.leaf = nullptr;
+			~Stem() 
+			{
+			}
+
+			inline Index left() const
+			{
+				return children[0];
+			}
+			inline Index right() const
+			{
+				return children[1];
+			}
+
+
+
+			inline bool is_upper(T3D const& p) const { return p[splitAxis] > splitValue; }
+			bool IsLeaf() const { return false; }
+
+			void Add(T3D const& p, int bucketsize, ::std::vector<Stem>& stems, ::std::vector<Leaf>& leafs)
+			{
+				auto const c = int(is_upper(p));
+				auto& child = children[c];
+
+				add(p, child, bucketsize, stems, leafs);
+
+			}
+
+			static inline void add(
+				T3D const& p
+				, Index& child
+				, int bucketsize
+				, ::std::vector<Stem>& stems
+				, ::std::vector<Leaf>& leafs)
+			{
+				if (child.is_leaf())
+				{
+					auto& leaf = leafs[child.from_leaf()];
+
+					auto needs_split = !leaf.Add(p, bucketsize);
+					if (needs_split)
+					{
+						//we need to split
+						assert(leaf.bucket.size() >= bucketsize);
+
+						T range;
+						auto const splitAxis = leaf.GetSplitAxis(range);
+						//only split if points extend in some direction at least EPSILON
+						if (range > ::std::numeric_limits<T>::epsilon())
+						{
+							auto const ileaf = child;
+							child = Index::make_stem(stems.size());
+
+							stems.emplace_back(splitAxis, range, leaf, ileaf, bucketsize, stems, leafs); //make new stem
+						}
+						//otherwise just keep this leaf
+					}
+				}
+				else
+				{
+					stems[child.from_stem()].Add(p, bucketsize, stems, leafs);
+				}
 			}
 
 		};
 
+		
 
 		int bucketsize;
-
-
-		Leaf* leaf;
-		Stem* stem;
+		::std::vector<Stem> stems;
+		::std::vector<Leaf> leafs;
+		Index child;
 
 	public:
 
-		KdTree(int bucketsize = 16)
+		KdTree(int bucketsize = 16, int default_nodes = 256)
 			: bucketsize(bucketsize)
-			, stem(NULL)
-			, leaf(new Leaf(bucketsize))
 		{
 			assert(bucketsize > 0);
 			assert(D > 0);
+
+			stems.reserve(default_nodes);
+			leafs.reserve(default_nodes);
+
+			child = Index::make_leaf(leafs.size());
+			leafs.emplace_back(bucketsize);
 		}
 
-		///**
-		//* Constructor for child nodes. Internal use only.
-		//*/
-		//KdTree(Leaf* leaf, int bucketsize)
-		//	: bucketsize(bucketsize)
-		//	, stem(NULL)
-		//	, leaf(leaf)
-		//{
-		//}
 
-
-		~KdTree() {
-			if (leaf) {
-				delete leaf;
-			}
-			else
-			{
-				delete stem;
-			}
+		virtual ~KdTree() 
+		{
 		}
 
 		void Add(T3D const& p)
 		{
-			if (leaf)
-			{
-				leaf->Add(p, *this);
-			}
-			else
-			{
-				stem->Add(p);
-			}
-
-		}
-
-		typedef KdTreeSearch<D, T, T3D> Search;
-
-		Search GetSearch()
-		{
-			return Search(this);
+			Stem::add(p, child, bucketsize, stems, leafs);
 		}
 
 		Self(Self&& o)
 			: bucketsize(::std::move( o.bucketsize))
-			, leaf(::std::move(o.leaf))
-			, stem(::std::move(o.stem))
+			, leafs(::std::move(o.leafs))
+			, stems(::std::move(o.stems))
+			, child(::std::move(o.child))
 		{
 		}
 
 		Self& operator=(Self&& o)
 		{
 			::std::swap(bucketsize, o.bucketsize);
-			::std::swap(leaf, o.leaf);
-			::std::swap(stem, o.stem);
+			::std::swap(leafs, o.leafs);
+			::std::swap(stems, o.stems);
+			::std::swap(child, o.child);
 		}
 
-		private:
-			Self(Self const& o);
-			Self& operator=(Self const& o);
+	private:
+		Self(Self const& o);
+		Self& operator=(Self const& o);
 
 
-	};
-
-	template <int D, typename T, typename T3D>
-	class KdTreeSearch {
-		typedef KdTree<D, T, T3D> Tree;
-		typedef KdTreeSearch<D, T, T3D> Self;
-
-		//only used for speedup
-		mutable std::vector<float> searchMem;
-		Tree const* tree;
 
 	public:
-		KdTreeSearch(Tree const* tree)
-			: searchMem(tree->bucketsize)
-			, tree(tree)
-		{}
+		class Search {
+			typedef Search Self;
 
-		//T3D NearestNeighbour(T3D const& p) const
-		//{
-		//	T3D r;
-		//	return
-		//}
+			//only used for speedup
+			mutable std::vector<float> searchMem;
+			Tree const* tree;
 
-		T3D NearestNeighbour(T3D const& p) const
-		{
-			T3D nearest;
-			T minDist = FLT_MAX;
+		public:
+			Search(Tree const* tree)
+				: searchMem(tree->bucketsize)
+				, tree(tree)
+			{}
 
-			std::vector<Tree const*> stack;
-			stack.reserve(256);
-			stack.push_back(tree);
-			while (!stack.empty())
+			//T3D NearestNeighbour(T3D const& p) const
+			//{
+			//	T3D r;
+			//	return
+			//}
+
+			T3D NearestNeighbour(T3D const& p, T minDist = ::std::numeric_limits<T>::max()) const
 			{
-				Tree const* const tree = stack.back();
-				stack.pop_back();
+				T3D nearest;
 
-				if (tree->leaf)
+				if (tree->child.is_leaf())
 				{
+					int i = search_leaf(p, tree->leafs[tree->child.from_leaf()], minDist, nearest);
+					return nearest;
+				}
 
-					assert(tree->leaf->bucket.size() <= tree->bucketsize);
-					int const numPoints = tree->leaf->bucket.size();
-					if (searchMem.size() < numPoints)
+				std::vector<Stem const*> stack;
+				stack.reserve(256);
+
+				stack.push_back(&tree->stems[tree->child.from_stem()]);
+				while (!stack.empty())
+				{
+					auto const& stem = *stack.back();
+					stack.pop_back();
+
 					{
-						searchMem.resize(numPoints);
-					}
-
-					int nearestI = -1;
-					//helper::FindNearest<T3D>(&(tree->leaf->bucket[0]), numPoints, p, 1, &i, &d, &(searchMem[0]));
-
-					auto& bucket = tree->leaf->bucket;
-					int i = 0;
-					for (auto& o : bucket)
-					{
-						auto dist = distance(p, o);
-
-						if (dist < minDist) {
-							minDist = dist;
-							nearestI = i;
+						float splitDist = p[stem.splitAxis] - stem.splitValue;
+						auto nearestChild  = stem.left();
+						auto furthestChild = stem.right();
+						//IsLeft()
+						if (splitDist < 0) {
+							::std::swap(nearestChild, furthestChild);
+							splitDist = -splitDist;
 						}
 
-						++i;
-					}
-					if (nearestI >= 0)
-					{
-						nearest = bucket[nearestI];
-					}
 
+						if (furthestChild.is_leaf())
+						{
+							int i = search_leaf(p, tree->leafs[furthestChild.from_leaf()], minDist, nearest);
+						}
+						else
+						{
+							//discard further child if distance of p to split plane is already larger than the best neighbour found so far
+							//if (splitDist < minDist)
+							{
+								stack.push_back(&tree->stems[furthestChild.from_stem()]);
+							}
+						}
 
+						if (nearestChild.is_leaf())
+						{
+							int i = search_leaf(p, tree->leafs[nearestChild.from_leaf()], minDist, nearest);
+						}
+						else
+						{
+							//nearest child is always inside
+							stack.push_back(&tree->stems[nearestChild.from_stem()]);
+						}
+					}
 				}
-				else
-				{
-					auto const* const stem = tree->stem;
-
-					float splitDist = p[stem->splitAxis] - stem->splitValue;
-					Tree* nearestChild;
-					Tree* furthestChild;
-					//IsLeft()
-					if (splitDist < 0) {
-						nearestChild = stem->left;
-						furthestChild = stem->right;
-						splitDist = -splitDist;
-					}
-					else
-					{
-						nearestChild = stem->right;
-						furthestChild = stem->left;
-					}
-
-					//discard further child if distance of p to split plane is already larger than the best neighbour found so far
-					if (splitDist < minDist)
-					{
-						stack.push_back(furthestChild);
-					}
-					//nearest child is always inside
-					stack.push_back(nearestChild);
-				}
-			}
-			return nearest;
+				return nearest;
 
 #if 0
-			auto curNode = this;
-			float dist = 0;
-			float maxDist = FLT_MAX;
-			while (dist < maxDist)
-			{
-				//down traversal
-				while (!curNode.leaf)
+				auto curNode = this;
+				float dist = 0;
+				float maxDist = FLT_MAX;
+				while (dist < maxDist)
 				{
-					Stem const* const stem = tree->stem;
-
-					float splitDist = p[stem->splitAxis] - stem->splitValue;
-					Tree* nearestChild;
-					Tree* furthestChild;
-					//IsLeft()
-					if (splitDist < 0) {
-						curNode = stem->left;
-					}
-					else
+					//down traversal
+					while (!curNode.leaf)
 					{
-						curNode = stem->right;
-					}
+						Stem const* const stem = tree->stem;
 
-					//discard further child if distance of p to split plane is already larger than the best neighbour found so far
-					if (splitDist < minDist)
-					{
-						stack.push_back(furthestChild);
-					}
-					//nearest child is always inside
-					stack.push_back(nearestChild);
+						float splitDist = p[stem->splitAxis] - stem->splitValue;
+						Tree* nearestChild;
+						Tree* furthestChild;
+						//IsLeft()
+						if (splitDist < 0) {
+							curNode = stem->left;
+						}
+						else
+						{
+							curNode = stem->right;
+						}
 
+						//discard further child if distance of p to split plane is already larger than the best neighbour found so far
+						if (splitDist < minDist)
+						{
+							stack.push_back(furthestChild);
+						}
+						//nearest child is always inside
+						stack.push_back(nearestChild);
+
+					}
 				}
-			}
 #endif
+			}
+
+			static inline int search_leaf(T3D const& p, Leaf const& leaf, T& minDist, T3D& nearest)
+			{
+				//assert(leaf.bucket.size() <= tree->bucketsize);
+				int nearestI = -1;
+
+				int i = 0;
+				for (auto& o : leaf.bucket)
+				{
+					auto dist = distance(p, o);
+
+					if (dist < minDist) {
+						minDist = dist;
+						nearestI = i;
+					}
+
+					++i;
+				}
+				if (nearestI >= 0)
+				{
+					nearest = leaf.bucket[nearestI];
+				}
+				return nearestI;
+			}
+
+			Self(Self&& o)
+				: tree(o.tree)
+				, searchMem(o.searchMem)
+			{
+			}
+
+			Self& operator=(Self&& o)
+			{
+				::std::swap(tree, o.tree);
+				::std::swap(searchMem, o.searchMem);
+			}
+
+		};
+
+		Search GetSearch()
+		{
+			return Search(this);
 		}
 
-		Self(Self&& o)
-			: tree(o.tree)
-			, searchMem(o.searchMem)
-		{
-		}
-
-		Self& operator=(Self&& o)
-		{
-			::std::swap(tree, o.tree);
-			::std::swap(searchMem, o.searchMem);
-		}
 
 	};
+
+
 }
